5eae409c9472eea8e28cf951595b49eca0a02856
Ernesto <ebastidasp@unal.edu.co>
2020-10-16 16:22:21 -0500

    Home View

--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -16,6 +16,12 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import logo from '../images/Geosmartlogo.png';
+import logobw from '../images/BWLogo.png';
+import tierra from '../images/Tierra.jpg';
+import { useHistory } from "react-router-dom";
+
+
+
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -23,36 +29,72 @@ const useStyles = makeStyles((theme) => ({
     alignItems: 'center',
     background: 'blue',
     border: 'black',
-    'border-width': 'medium',
-    'margin-top': '80px',
-    background: 'rgba(255, 255, 255, 0.8)',
-    'border-radius': '20px'
+    'margin-top': '200px',
+    background: 'darkblue',
+    'border-radius': '40px'
   },
   item: {
     // background: 'red',
     width: '100%',
-    background: 'blue',
+    background: 'lightblue',
     'textAlign': 'center',
     'borderRadius': '5px',
-    'marginTop': '220px'
+    'marginTop': '120px'
   },
   center: {
     display: 'center',
     justifyContent: 'center',
     'marginLeft': '700px',
-    width: '250px'
+    width: '250px',
+    'float': 'left'
+  },
+  table:{
+    width:'100%',
+    display:'table'
+  },
+  top:{
+    display: 'table-cell',
+    position: 'relative',
+    width:'100%',
+    height: '100px',
+    'background-color': 'rgba(89,144,222,.6)',
+    'vertical-align':'middle'
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
   },
 }));
 
+
+
 const HomePage = () => {
+
+  let history = useHistory();
+  const routeChange1 = () =>{ 
+    let path = `./login`; 
+    history.push(path);
+  }
+  
+  const routeChange2 = () =>{ 
+    let path = `./signup`; 
+    history.push(path);
+  }
+
+  const show1 = () =>{
+
+  }
 
   const classes = useStyles();
   return (
     <Grid
+    container
     className={classes.container}
-    direction="column"
-    spacing={2}
   >
+    <font color = "white" size = "80">
+      <h1>
+        Welcome to Geosmart app.
+      </h1>
+    </font>
     <Grid
       item
       sm={12}
@@ -60,8 +102,25 @@ const HomePage = () => {
     >
       
     <div className ={classes.center}>
-        <img src={logo} alt="Logo"  />
+        <img src={logo} alt="Logo" width="308" height="300" 
+                onClick={routeChange1} />
     </div>
+    <div className ={classes.center}>
+        <img src={logobw} alt="Logo2" width="328" height="320" 
+                onClick={routeChange2} 
+                onmouseover={<p>Signup</p>}/>
+    </div>
+    
+    </Grid>
+    <Grid>
+      <font color = "white" face = "Garamond">
+        <h2 >
+          Learn Geography by Playing!
+        </h2>
+        <h2 >
+          Click the left logo to login, or the right logo to sign up.
+        </h2>
+      </font>
     </Grid>
   </Grid>
   );
